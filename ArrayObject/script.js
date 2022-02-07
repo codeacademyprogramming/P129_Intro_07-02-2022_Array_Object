3a3d864ff409c2af7f6da70bbfeef47342c7db04
HamidMammadov <hamidvm@code.edu.az>
2022-02-07 12:54:00 +0400

    First Commit

---
+++ ArrayObject/script.js
@@ -0,0 +1,133 @@
+// let name1 = "Perviz";
+// let name2 = "Hamid";
+
+// let arr = ["Parviz","Hamid","Alyasar","Ceyhun","Elgiz"]
+
+// console.log(arr[0])
+// console.log(arr[1])
+// console.log(arr[2])
+// console.log(arr[3])
+// console.log(arr[4])
+
+// let arr = [];
+
+// arr[5] = "Hamid";
+
+// console.log(arr[0]);
+// console.log(arr[5]);
+
+// let students = [];
+// let j = 0;
+
+// while(true)
+// {
+//     let name = prompt("Telebe Adini Daxil Et: ")
+//     students[j] = name;
+//     j++;
+//     let check = confirm("Davam Etmek Isdeyirsinizmi: ");
+//     if(check != true){
+//         break;
+//     }
+// }
+
+// let variablename = students.length;
+// let lastindex = variablename - 1;
+
+// console.log(students[students.length-1]);
+
+// for (let i = 0; i < students.length; i++) {
+//     console.log(students[i]);
+// }
+
+
+// let numbers = [5,7,15,17];
+
+// let a = numbers[2];
+
+// console.log(a);
+// console.log(numbers[2]);
+
+// let student = 
+// {
+//     name : "Hamid",
+//     surname : "Mammadov",
+//     age : 31,
+//     isMarred : true
+// }
+
+// let student1 = 
+// {
+//     name : "Hamid",
+//     surname : "Mammadov",
+//     age : 31,
+//     isMarred : true
+// }
+
+// let student2 = 
+// {
+//     name : "Parviz",
+//     surname : "Mammadov",
+//     age : 19,
+//     isMarred : true
+// }
+
+// let name1 = "hamid";
+
+// let surname1 = "Mammadov";
+
+// let age1 = 31;
+
+// let name2 = "Perviz";
+// let Group = [student,student1,student2]
+
+// console.log(student.name +" "+student.surname);
+
+// Group[Group.length-1].surname = "Alizade"
+
+// console.log(Group[Group.length-1].name+' '+Group[Group.length-1].age +' '+Group[Group.length-1].surname)
+
+// console.log(Group[2].name+' '+Group[2].age +' '+Group[2].surname);
+
+
+//Start
+let numbers = [5,7,12,15,18,24,30];
+
+for (let i = 0; i < numbers.length; i++) {
+
+    if(numbers[i] % 6 == 0)
+    {
+        console.log(numbers[i]);
+    }
+}
+//END
+
+
+// a1 = numbers[1];
+// if(a1 % 6 == 0){
+//     console.log(a1);
+// }
+
+// a1 = numbers[2];
+// if(a1 % 6 == 0){
+//     console.log(a1);
+// }
+
+// a1 = numbers[3];
+// if(a1 % 6 == 0){
+//     console.log(a1);
+// }
+
+// a1 = numbers[4];
+// if(a1 % 6 == 0){
+//     console.log(a1);
+// }
+
+// a1 = numbers[5];
+// if(a1 % 6 == 0){
+//     console.log(a1);
+// }
+
+// a1 = numbers[6];
+// if(a1 % 6 == 0){
+//     console.log(a1);
+// }
